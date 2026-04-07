7aa69d9de1e48aefdc670da183f11033fae32b5a
matrix12345695-afk <matrix12345695@gmail.com>
2026-04-07 16:51:56 +0500

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import logging
 from bot import main as bot_main
 from app import app
 
-def run_bot():
-    print("🔥 BOT STARTING...")
-    asyncio.run(bot_main())
-
 def run_web():
     app.run(host="0.0.0.0", port=10000)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    threading.Thread(target=run_bot).start()
-    run_web()
+
+    # Flask в отдельном потоке
+    threading.Thread(target=run_web).start()
+
+    # Бот в главном потоке (ВАЖНО)
+    print("🔥 BOT STARTING...")
+    asyncio.run(bot_main())
